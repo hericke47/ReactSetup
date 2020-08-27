3b7e0b95893e6aa05aa663e6b6fe1027988958a0
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-27 15:10:00 -0300

    configurando styled-components e fazendo um estilo global simples

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { AppProps } from 'next/app' // para o componente entender que ele recebe as props do component
 
+import GlobalStyle from '../styles/global'
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
 }
 
 export default MyApp
