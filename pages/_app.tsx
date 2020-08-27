bafe33478d6b00d25ae93a43d7a8cc4a382c94eb
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-27 14:09:42 -0300

    adicionando prettier e removendo os erros

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
-function MyApp ({ Component, pageProps }) {
+import React from 'react'
+import { AppProps } from 'next/app' // para o componente entender que ele recebe as props do component
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
