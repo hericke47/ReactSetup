5cccfed9a84e0349096a35bf26dd242ce394e214
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-28 13:23:27 -0300

    adicionando favicon e fonte Roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
-import Document, { DocumentInitialProps, DocumentContext } from 'next/document'
+import Document, {
+  DocumentInitialProps,
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -29,5 +36,27 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="icon" href="/space.ico" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
